16184970d8c1393029c563d20b7c7644151529a9
ragul023 <ragul4172@gmail.com>
2026-06-02 14:28:32 +0530

    updated sql file

--- Leatask1/Backend/SQL/sqlcode.sql
+++ Leatask1/Backend/SQL/sqlcode.sql
@@ -1,15 +1,65 @@
-CREATE TABLE form_fields (
+CREATE DATABASE leatasks;
+USE leatasks;
 
+CREATE TABLE colleges (
+    college_code INT PRIMARY KEY,
+    college_name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE collegedetails (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    college_code INT NOT NULL,
+    field_lable VARCHAR(200) NOT NULL,
+    field_value VARCHAR(500),
+
+    FOREIGN KEY (college_code)
+    REFERENCES colleges(college_code)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE hosteldetails (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    college_code INT NOT NULL,
+    field_lable VARCHAR(200) NOT NULL,
+
+    boys_value VARCHAR(200) NULL,
+    girls_value VARCHAR(200) NULL,
+
+    single_value VARCHAR(200) NULL,
+
+    FOREIGN KEY (college_code)
+    REFERENCES colleges(college_code)
+    ON DELETE CASCADE
+);
+
+create table bankdetails(
+id int auto_increment primary key,
+college_code INT NOT NULL,
+field_lable VARCHAR(200) NOT NULL,
+field_value VARCHAR(500) null,
+
+    FOREIGN KEY (college_code)
+    REFERENCES colleges(college_code)
+    ON DELETE CASCADE
+);
+
+
+CREATE TABLE branchdetails (
     id INT AUTO_INCREMENT PRIMARY KEY,
 
-    submission_id INT,
+    college_code INT NOT NULL,
 
-    field_name VARCHAR(255),
+    branch_code VARCHAR(50) NOT NULL,
 
-    field_label VARCHAR(255),
+    approved_intake INT NOT NULL,
 
-    field_value TEXT,
+    course_start_year YEAR,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    nba_accredited varchar(100),
 
+    valid_year YEAR NULL,
+
+    FOREIGN KEY (college_code)
+    REFERENCES colleges(college_code)
+    ON DELETE CASCADE
 );
